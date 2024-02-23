1674572ab86b88a467f1f1dd10451da8738af2b4
MattManamtam <manamtammatttheodore@gmail.com>
2024-02-23 21:04:57 +0800

    commit the one-to-one

---
+++ student-app/resources/views/components/app-web-layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CRUD WEB</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/styles.css"> 
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light py-3 shadow-sm"> <div class="container"> <a class="navbar-brand font-weight-bold" href="#">CRUD </a>  
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav"> </div> 
+        </div>
+    </nav>
+
+    <div class="container mt-5">  @yield('content')
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
